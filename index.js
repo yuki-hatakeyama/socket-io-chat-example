38dd7ff4273bdd71398e025cd4dec2ebfc84f046
yuki-hatakeyama <yuki-hatakeyama@two2.jp>
2024-04-02 17:38:53 +0900

    Server delivery

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
 
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+// open the database file
+const db = await open({
+  filename: "chat.db",
+  driver: sqlite3.Database,
+});
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
+
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -19,13 +36,35 @@ app.get("/", (req, res) => {
 });
 
 //
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-
+io.on("connection", async (socket) => {
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      // store the message in the database
+      result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
+    } catch (e) {
+      // TODO handle the failure
+      return;
+    }
     // 全員にイベントを送信するためのメソッド
-    io.emit("chat message", msg);
+    // include the offset with the message
+    io.emit("chat message", msg, result.lastID);
   });
+
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+      await db.each(
+        "SELECT id, content FROM messages WHERE id > ?",
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit("chat message", row.content, row.id);
+        }
+      );
+    } catch (e) {
+      // something went wrong
+    }
+  }
 });
 
 server.listen(3000, () => {
